687c6c02f3fca84df2b16700a99f4bfcfb541743
Michalis Kamburelis <michalis.kambi@gmail.com>
2009-01-27 20:10:09 +0000

    * renamed test_program_compilation.sh to better compile_all_programs.sh * all compile*.sh scripts and castle/Makefile honour now KAMBI_FPC_OPTIONS variable, useful for cross-compiling
    git-svn-id: https://svn.code.sf.net/p/castle-engine/code/trunk/malfunction@4199 afe8dcc5-9094-451b-8773-5f495042a737

--- compile_unix.sh
+++ compile_unix.sh
@@ -25,4 +25,4 @@ rm -f opengl/glwindow.o \
       opengl/GLWindow.o \
       opengl/GLWindow.ppu
 
-fpc -dRELEASE @kambi.cfg -dGLWINDOW_XLIB ../malfunction/malfunction.pasprogram
+fpc -dRELEASE "${KAMBI_FPC_OPTIONS:-}" @kambi.cfg -dGLWINDOW_XLIB ../malfunction/malfunction.pasprogram
--- compile_windows.sh
+++ compile_windows.sh
@@ -25,4 +25,4 @@ rm -f opengl/glwindow.o \
       opengl/GLWindow.o \
       opengl/GLWindow.ppu
 
-fpc -dRELEASE @kambi.cfg ../malfunction/malfunction.pasprogram
+fpc -dRELEASE "${KAMBI_FPC_OPTIONS:-}" @kambi.cfg ../malfunction/malfunction.pasprogram
