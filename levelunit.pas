43cf95b91d24a4ebbe14d28d4ff05d1c9a39fbd2
Michalis Kamburelis <michalis.kambi@gmail.com>
2012-09-23 19:44:26 +0000

    Rename unit name CastleShape to CastleShapes. Since we renamed in anyway (Shape->CastleShape), make it consistent (plural) with other names. You usually have many shapes.
    git-svn-id: https://svn.code.sf.net/p/castle-engine/code/trunk/malfunction@11881 afe8dcc5-9094-451b-8773-5f495042a737

--- levelunit.pas
+++ levelunit.pas
@@ -91,7 +91,7 @@ unit LevelUnit;
 interface
 
 uses SysUtils, GameGeneral, CastleWindow, CastleScene, X3DFields, X3DNodes,
-  CastleClassUtils, Boxes3D, CastleShape, CastleGLUtils;
+  CastleClassUtils, CastleShapes, Boxes3D, CastleGLUtils;
 
 type
   { levelType wplywa na wiele rzeczy. Ponizej bede dokumentowal sobie
