a3b4987bbd48bd50cad1ad52ca27d09bf6e66001
Michalis Kamburelis <michalis.kambi@gmail.com>
2013-08-15 21:01:21 +0000

    Fix malfunction to work without depending that raster pos is multiplied by matrix.
    git-svn-id: https://svn.code.sf.net/p/castle-engine/code/trunk/malfunction@13038 afe8dcc5-9094-451b-8773-5f495042a737

--- modemenuunit.pas
+++ modemenuunit.pas
@@ -27,8 +27,9 @@ interface
 implementation
 
 uses GL, SysUtils, CastleWindow, GameGeneral, CastleGLBitmapFonts,
-  CastleBitmapFont_Isuckatgolf_m32, CastleGLUtils, CastleMessages, LevelUnit, CastleImages,
-  CastleVectors, CastleUtils, CastleGLImages, CastleColors, CastleUIControls, CastleKeysMouse;
+  CastleBitmapFont_Isuckatgolf_m32, CastleGLUtils, CastleMessages, LevelUnit,
+  CastleImages, CastleVectors, CastleUtils, CastleGLImages, CastleColors,
+  CastleUIControls, CastleKeysMouse;
 
 { module consts and vars ---------------------------------------------------- }
 
@@ -44,9 +45,10 @@ const
     'Play - "fog & mountains" level',
     'Quit');
 
-var currentMenu: TMenuItem = Low(TMenuItem);
-    listBg: TGLImage;
-    menuFont: TGLBitmapFont;
+var
+  currentMenu: TMenuItem = Low(TMenuItem);
+  listBg: TGLImage;
+  menuFont: TGLBitmapFont;
 
 { mode enter/exit ----------------------------------------------------------- }
 
@@ -55,120 +57,113 @@ procedure Press(Window: TCastleWindowBase; const Event: TInputPressRelease); for
 
 procedure modeEnter;
 begin
- OrthoProjection(0, Window.width, 0, Window.height);
- Window.OnDraw := @draw;
- Window.OnPress := @Press;
+  OrthoProjection(0, Window.width, 0, Window.height);
+  Window.OnDraw := @draw;
+  Window.OnPress := @Press;
 end;
 
 procedure modeExit;
 begin
 end;
 
-{ glw callbacks ----------------------------------------------------------- }
+{ window callbacks ----------------------------------------------------------- }
 
 procedure draw(Window: TCastleWindowBase);
-var mi: TMenuItem;
-{const SLower = 'by Michalis Kamburelis';}
+var
+  mi: TMenuItem;
+  X, Y: Integer;
 begin
- glLoadIdentity;
- listBg.Draw(0, 0);
+  listBg.Draw(0, 0);
 
- glTranslatef(Window.width*50 div 640, Window.height*350 div 480, 0);
- for mi := Low(mi) to High(mi) do
- begin
-  glPushMatrix;
-    glTranslatef(0, -Ord(mi)*(menufont.RowHeight+10), 0);
+  X := Window.width*50 div 640;
+  Y := Window.height*350 div 480;
+  for mi := Low(mi) to High(mi) do
+  begin
+    Y -= menufont.RowHeight+10;
 
     if mi = currentMenu then
     begin
-     GLRectangleBorder(-10, -menufont.Descend,
-       menufont.TextWidth(menuNames[mi])+10, menuFont.RowHeight,
-       White4Single);
-     glColorv(Yellow3Single);
+      GLRectangleBorder(X - 10, Y - menufont.Descend,
+        X + menufont.TextWidth(menuNames[mi])+10, Y + menuFont.RowHeight,
+        White4Single);
+      glColorv(Yellow3Single);
     end else
-     glColorv(White3Single);
+      glColorv(White3Single);
 
-    glRasterPos2i(0, 0);
-    menufont.print(menuNames[mi]);
-  glPopMatrix;
- end;
-
-{ glLoadIdentity;
- glColorv(Yellow3f);
- glRasterPos2i((Window.width-courierFont.TextWidth(SLower))div 2,
-	       courierFont.Descend+5);
- courierFont.print(SLower);}
+    SetWindowPos(X, Y);
+    menufont.PrintAndMove(menuNames[mi]);
+  end;
 end;
 
 procedure Press(Window: TCastleWindowBase; const Event: TInputPressRelease);
 begin
- if Event.EventType <> itKey then Exit;
- case Event.key of
-  K_Up:
-    begin
-     if currentMenu = Low(currentMenu) then
-      currentMenu := High(currentMenu) else
-      currentMenu := Pred(currentMenu);
-     Window.PostRedisplay;
-    end;
-  K_Down:
-    begin
-     if currentMenu = High(currentMenu) then
-      currentMenu := Low(currentMenu) else
-      currentMenu := Succ(currentMenu);
-     Window.PostRedisplay;
-    end;
-  K_Enter:
-    case currentMenu of
-     miGameManual:
-       MessageOK(Window,
-         'If you want, you can dream that you''re a saviour of galaxy or' +
-         ' something like that. The truth is:' +nl+
-         '1. You sit inside the most junky and malfunctioning space ship in the whole universe.' +nl+
-         '2. Noone knows what''s going on but there are some ' +
-         'freakin'' ALIEN SPACESHIPS everywhere around, and THEY JUST GOT' +
-         ' DOWN ON YOU.' +nl+
-         nl+
-         'Keys :' +nl+
-         '  Arrows = rotate' +nl+
-         '  A/Z = increase/decrease speed ' +
-         '(note that your spaceship can also fly backwards if you decrease '+
-         'your speed too much)' +nl+
-         '  Space = fire rocket !' +nl+
-         '  C = crosshair on/off' +nl+
-         '  R = radar on/off' +nl+
-         '  Esc = exit to menu' +nl+
-         '  F5 = save screen to PNG' +nl+
-         nl+
-         'There is only one goal: destroy all enemy ships on every level.' +nl+
-         nl+
-         SCastleEngineProgramHelpSuffix(DisplayApplicationName, Version, false),
-	 taLeft);
-     miPlaySunnyDay: PlayGame(vrmlsDir +'lake.wrl');
-     miPlayDeepSpace: PlayGame(vrmlsDir +'mobius.wrl');
-     miPlayRainMountains: PlayGame(vrmlsDir +'wawoz.wrl');
-     miQuit:
-       if MessageYesNo(Window, 'Are you sure you want to quit ?') then Window.close;
-    end;
- end;
+  if Event.EventType <> itKey then Exit;
+  case Event.key of
+    K_Up:
+      begin
+        if currentMenu = Low(currentMenu) then
+          currentMenu := High(currentMenu) else
+          currentMenu := Pred(currentMenu);
+        Window.PostRedisplay;
+      end;
+    K_Down:
+      begin
+        if currentMenu = High(currentMenu) then
+          currentMenu := Low(currentMenu) else
+          currentMenu := Succ(currentMenu);
+        Window.PostRedisplay;
+      end;
+    K_Enter:
+      case currentMenu of
+        miGameManual:
+          MessageOK(Window,
+            'If you want, you can dream that you''re a saviour of galaxy or' +
+            ' something like that. The truth is:' +nl+
+            '1. You sit inside the most junky and malfunctioning space ship in the whole universe.' +nl+
+            '2. Noone knows what''s going on but there are some ' +
+            'freakin'' ALIEN SPACESHIPS everywhere around, and THEY JUST GOT' +
+            ' DOWN ON YOU.' +nl+
+            nl+
+            'Keys :' +nl+
+            '  Arrows = rotate' +nl+
+            '  A/Z = increase/decrease speed ' +
+            '(note that your spaceship can also fly backwards if you decrease '+
+            'your speed too much)' +nl+
+            '  Space = fire rocket !' +nl+
+            '  C = crosshair on/off' +nl+
+            '  R = radar on/off' +nl+
+            '  Esc = exit to menu' +nl+
+            '  F5 = save screen to PNG' +nl+
+            nl+
+            'There is only one goal: destroy all enemy ships on every level.' +nl+
+            nl+
+            SCastleEngineProgramHelpSuffix(DisplayApplicationName, Version, false),
+            taLeft);
+        miPlaySunnyDay: PlayGame(vrmlsDir +'lake.wrl');
+        miPlayDeepSpace: PlayGame(vrmlsDir +'mobius.wrl');
+        miPlayRainMountains: PlayGame(vrmlsDir +'wawoz.wrl');
+        miQuit:
+          if MessageYesNo(Window, 'Are you sure you want to quit ?') then Window.close;
+      end;
+  end;
 end;
 
 procedure WindowOpen(const Container: IUIContainer);
 begin
- listBg := TGLImage.Create(imagesDir +'menubg.png', [TRGBImage],
-   Window.width, Window.height, riBilinear);
- menuFont := TGLBitmapFont.Create(BitmapFont_Isuckatgolf_m32);
+  listBg := TGLImage.Create(imagesDir +'menubg.png', [TRGBImage],
+    Window.width, Window.height, riBilinear);
+  menuFont := TGLBitmapFont.Create(BitmapFont_Isuckatgolf_m32);
 end;
 
 procedure WindowClose(const Container: IUIContainer);
 begin
- FreeAndNil(menuFont);
- FreeAndNil(ListBG);
+  FreeAndNil(menuFont);
+  FreeAndNil(ListBG);
 end;
 
 initialization
- gameModeEnter[modeMenu] := @modeEnter;
- gameModeExit[modeMenu] := @modeExit;
- OnGLContextOpen.Add(@WindowOpen);
- OnGLContextClose.Add(@WindowClose);
+  gameModeEnter[modeMenu] := @modeEnter;
+  gameModeExit[modeMenu] := @modeExit;
+  OnGLContextOpen.Add(@WindowOpen);
+  OnGLContextClose.Add(@WindowClose);
 end.
